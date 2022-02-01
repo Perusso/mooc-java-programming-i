e5394aad6d38172917de0959ffb5f29c39acccd4
mperusso <76129749+Perusso@users.noreply.github.com>
2022-01-31 22:30:04 -0300

    Add files via upload

---
+++ part05-Part05_02.Book/src/main/java/Book.java
@@ -0,0 +1,41 @@
+public class Book {
+
+    private String author;
+    private String name;
+    private int pages;
+
+    public Book(String author, String name, int pages) {
+        this.author = author;
+        this.name = name;
+        this.pages = pages;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPages() {
+        return pages;
+    }
+
+    public void setPages(int pages) {
+        this.pages = pages;
+    }
+
+    @Override
+    public String toString() {
+        return this.author + ", " + this.name + ", " + this.pages + " pages";
+    }
+}
